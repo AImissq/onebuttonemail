9760fa045d8a4c323cbe6225480dba7710954709
Tommy Hodgins <tomhodgins@gmail.com>
2013-09-18 10:19:56 -0400

    Adding code from staticresource.com

---
+++ script/sendmail.php
@@ -0,0 +1,15 @@
+<?php 
+    $to = $_POST["mailto"]; 
+    $from = "noreply@staticresource.com"; 
+    $subject = $_POST["subject"]; 
+    $message = $_POST["message"];
+
+    $headers  = "From: $from\r\n"; 
+    $headers .= "Content-type: text/html\r\n"; 
+     
+    // now lets send the email. 
+    mail($to, $subject, $message, $headers); 
+
+		// once we have attempted to send the mail, show alert
+		header ('Location: ../?sent=1');
+?>
