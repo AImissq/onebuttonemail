650f69870de81ec83496dd401edab071b1273c5a
Tommy Hodgins <tomhodgins@gmail.com>
2014-04-17 13:48:50 -0400

    Clean up formatting

--- script/sendmail.php
+++ script/sendmail.php
@@ -1,15 +1,15 @@
-<?php 
-    $to = $_POST["mailto"]; 
-    $from = "noreply@staticresource.com"; 
-    $subject = $_POST["subject"]; 
+<?php
+    $to = $_POST["mailto"];
+    $from = "noreply@staticresource.com";
+    $subject = $_POST["subject"];
     $message = $_POST["message"];
 
-    $headers  = "From: $from\r\n"; 
-    $headers .= "Content-type: text/html\r\n"; 
-     
-    // now lets send the email. 
-    mail($to, $subject, $message, $headers); 
+    $headers  = "From: $from\r\n";
+    $headers .= "Content-type: text/html\r\n";
 
-		// once we have attempted to send the mail, show alert
-		header ('Location: ../?sent=1');
+    // now lets send the email
+    mail($to, $subject, $message, $headers);
+
+    // once we have attempted to send the mail, show alert
+    header ('Location: ../?sent=1');
 ?>
